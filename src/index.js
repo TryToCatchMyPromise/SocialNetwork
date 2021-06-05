a9247f34f523c6020dcda94ee999a5ae087578ca
Gleb Sapegin <gleb@inperium.com>
2021-06-05 12:00:01 +0300

    Deep copy task

--- src/index.js
+++ src/index.js
@@ -6,6 +6,34 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {App} from 'src/App'
 
+const obj = {
+  one: {
+    oneOne: 1,
+    oneTwo: 2
+  },
+  two: 2
+
+}
+
+const deepCopyFunction = (inObject) => {
+  let outObject = Array.isArray(inObject) ? [] : {}
+
+  if (typeof inObject !== 'object' || inObject === null || !inObject.hasOwnProperty) {
+    return inObject
+  }
+
+  for (let key in inObject) {
+    let value = inObject[key]
+
+    outObject[key] = deepCopyFunction(value)
+  }
+
+  return outObject
+}
+
+console.log("Copied obj: ", deepCopyFunction(obj))
+
+
   ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
